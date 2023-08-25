7c924f2bd52b507cad3ff574ccf9bd2525d3da37
Santosh Purbey <purbey.santosh@hotmail.com>
2023-08-25 19:19:30 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "spring-hat-31",
+  url: "https://spring-hat-31.botics.co",
+  api: "https://spring-hat-31.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
